432cdea33a2b56710d3caa39f88978ee7359f719
MustafaMosad <mustafamosad57@gmail.com>
2019-11-09 12:09:26 +0200

    Repository Annotation

--- pharma/src/main/java/com/appsstuff/pharma/repo/ConfirmationTokenRepository.java
+++ pharma/src/main/java/com/appsstuff/pharma/repo/ConfirmationTokenRepository.java
@@ -1,9 +1,11 @@
 package com.appsstuff.pharma.repo;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.appsstuff.pharma.security.model.ConfirmationToken;
 
+@Repository
 public interface ConfirmationTokenRepository extends CrudRepository<ConfirmationToken, String> {
 	ConfirmationToken findByConfirmationToken(String confirmationToken);
 }
